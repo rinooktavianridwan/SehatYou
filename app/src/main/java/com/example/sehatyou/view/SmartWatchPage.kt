0f6c2bf907b60111483db30a91371b04475553c2
Warwoyo <bobobobi.krn@gmail.com>
2024-11-26 15:06:01 +0700

    debug import R

--- app/src/main/java/com/example/sehatyou/view/SmartWatchPage.kt
+++ app/src/main/java/com/example/sehatyou/view/SmartWatchPage.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import com.example.sehatyou.R
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
