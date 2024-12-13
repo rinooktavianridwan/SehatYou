cd2adc4e4b547b068f4c4eea251c3abf4d4c51fa
Rino <rinooktavianridwan@gmail.com>
2024-12-13 22:12:04 +0700

    entity user

--- app/src/main/java/com/example/sehatyou/model/UserEntity.kt
+++ app/src/main/java/com/example/sehatyou/model/UserEntity.kt
@@ -1,9 +1,14 @@
 package com.example.sehatyou.model
 
+import androidx.room.PrimaryKey
+
 data class UserEntity(
-    val beratBadan: String = "",
-    val tinggiBadan: String = "",
-    val tglLahir: String = "",
-    val jamKerjaStart: String = "",
-    val jamKerjaEnd: String = ""
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    var kontak : String = "",
+    var beratBadan: String = "",
+    var tinggiBadan: String = "",
+    var tglLahir: String = "",
+    var jamKerjaStart: String = "",
+    var jamKerjaEnd: String = ""
 )
